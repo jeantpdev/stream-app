6bc6f0b9accbfefcb53bcef81048f8ba47530276
Jean <00zacks00@gmail.com>
2025-06-06 23:58:29 -0500

    fix: se cambian params de endpoints de int a uuid

--- routes/clients_routes.py
+++ routes/clients_routes.py
@@ -5,9 +5,10 @@ from controllers.clients_controller import ClientsController
 clients = Blueprint('clients', __name__)
 clients_controller = ClientsController()
 
-@clients.route('/revendedores/<int:revendedor_id>/clients', methods=['GET'])
+@clients.route('/revendedores/<uuid:revendedor_id>/clients', methods=['GET'])
 @cross_origin()
 def get_all_clients(revendedor_id):
+    print("HOLA")
     return clients_controller.get_all_clients(revendedor_id)
 
 @clients.route('/clients/<int:client_id>', methods=['GET'])
